67fa1445a110b45cdc1b7bec2e664f2ce74aa41e
Marco Lucio <marcoalucio15@gmail.com>
2025-03-25 17:29:50 -0600

    Created initial routing with protection.

---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div> Login Page for pathexplorer </div>
+  )
+}
+
+export default Login
---
+++ src/pages/manager/ManagerDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+/**
+ * Componente Dashboard para usuarios con rol de Manager
+ * @returns 
+ */
+export const ManagerDashboard = () => {
+  return (
+    <div>Manager Dashboard</div>
+  )
+}

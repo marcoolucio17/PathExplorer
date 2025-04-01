d4fe76a4d26c9c83ef4bf9b55e02ee147962a8fd
JUANECL314 <a01198418@tec.mx>
2025-04-01 15:18:54 -0600

    dashboard crear proyecto manager avance

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import { ManagerDashboard } from "./pages/manager/ManagerDashboard";
 import { ManagerPerfil } from "./pages/manager/ManagerPerfil";
 import { ManagerProyecto } from "./pages/manager/ManagerProyecto";
 import { ManagerVistaPerfil } from "./pages/manager/ManagerVistaPerfil";
+import { ManagerCrearProyecto1 } from "./pages/manager/CrearProyecto/ManagerCrearProyecto1";
 
 // Imports de TFS
 import { TFSDashboard } from "./pages/tfs/TFSDashboard";
@@ -33,7 +34,7 @@ function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/" element={<ManagerDashboard />} />
+        <Route path="/" element={<ManagerCrearProyecto1 />} />
         <Route path="unauthorized" element={<Unauthorized />} />
 
         {/* rutas de manager */}
@@ -43,6 +44,7 @@ function App() {
           <Route path="manager/perfil" element={<ManagerPerfil/>} />
           <Route path="manager/proyecto" element={<ManagerProyecto />} />
           <Route path="manager/vistaperfil" element={<ManagerVistaPerfil />} />
+          <Route path="manager/crearproyecto" element={<ManagerCrearProyecto1 />} />
         </Route>
 
         {/* rutas de tfs */}
---
+++ src/pages/manager/CrearProyecto/ManagerCrearProyecto1.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+
+import '../../empleado/EmpleadoDashboard.css'
+import './ManagerCrearProyecto1.css'
+
+
+export const ManagerCrearProyecto1 = () => {
+    return (
+        <div className="contenedor-crear-proyecto">
+            <div className="titulo-crear-proyecto">
+                <h1>Create a new project</h1>
+            </div>
+            <div className="formulario-crear-proyecto">
+                <div className="campo-crear-proyecto">
+                    <div className="titulo-campo-crear-proyecto">
+                        <h2>Project Name:</h2>
+                    </div>
+                    
+                    <input type="text" />
+                </div>
+                <div className="campo-crear-proyecto">
+                    <div className="titulo-campo-crear-proyecto">
+                        <h2>Client name:</h2>
+                    </div>
+                    <input type="text" />
+                </div>
+                <div className="campo-crear-proyecto">
+                    <div className="titulo-campo-crear-proyecto">
+                        <h2>Description:</h2>
+                    </div>
+                    <input type="text" />
+                </div>
+                <div className="campo-crear-proyecto">
+                    <div className="titulo-campo-crear-proyecto">
+                        <h2>General Requirements:</h2>
+                    </div>
+                    <input type="text" />
+                </div>
+                
+            </div>
+        </div>
+    )
+}

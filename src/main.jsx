9025a22edd9f6fc3aa4e588e5266b9f3a3b67a6a
Leonardo Pequeño Moreno <A01178029@tec.mx>
2025-03-31 19:33:46 -0600

    test

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import { EmpleadoPerfil } from "./pages/empleado/EmpleadoPerfil.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <EmpleadoPerfil />
     </StrictMode>
   </BrowserRouter>
 );
--- src/pages/empleado/EmpleadoPerfil.jsx
+++ src/pages/empleado/EmpleadoPerfil.jsx
@@ -1,7 +1,55 @@
 import React from 'react'
 
+import './EmpleadoPerfil.css'
+/**
+ * Componente dashboard para usuarios con rol de Empleado
+ * @returns 
+ */
 export const EmpleadoPerfil = () => {
   return (
-    <div>EmpleadoPerfil</div>
+    <div class = "contenedor-Dashboard">
+      <div class = "titulo-Dashboard">
+        <h1>My Project Dashboard</h1>
+        <br/>
+        <h2>Hello Perenganit, we compiled these projects for you:</h2>
+      </div>
+      <div class = "buscador-Dashboard">
+        <input type="text" placeholder="Search by keyword"></input>
+        <button class="buscar-boton"><i class="bi bi-search buscador"></i></button>
+        <div class = "buscador-espacio"></div>
+        <button class="filtro-boton"><i class="bi bi-caret-down-fill filtrador"></i></button>
+        <button class="filtro-boton"><i class="bi bi-funnel filtrador"></i></button>
+      </div>
+      <div class = "proyectos-Dashboard">
+        <div class ="proyecto">
+          <img src = "/images/3d_avatar_6.png" alt = "Avatar"></img>
+          <h1>Gansitos Project</h1>
+          <p>Java Developer</p>
+        </div>
+        <div class ="proyecto" >
+          <img src = "/images/3d_avatar_6.png" alt = "Avatar"></img>
+          <h1>Gansitos Project</h1>
+          <p>Java Developer</p>
+        </div>
+        <div class ="proyecto" >
+         <img src = "/images/3d_avatar_6.png" alt = "Avatar"></img>
+          <h1>Gansitos Project</h1>
+          <p>Java Developer</p>
+        </div>
+        <div class ="proyecto" >
+          <img src = "/images/3d_avatar_6.png" alt = "Avatar"></img>
+          <h1>Gansitos Project</h1>
+          <p>Java Developer</p>
+        </div>
+        <div class ="proyecto" >
+          <img src = "/images/3d_avatar_6.png" alt = "Avatar"></img>
+          <h1>Gansitos Project</h1>
+          <p>Java Developer</p>
+        </div>
+
+      </div>
+
+    </div>
+    
   )
 }

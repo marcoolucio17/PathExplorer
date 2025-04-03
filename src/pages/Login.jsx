2ba8eb5dee20a99444ae220959eef7b92acfc396
Marco Lucio <marcoalucio15@gmail.com>
2025-04-02 18:40:40 -0600

    Created & developed login page.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import "./../styles/Login.css"
+
+import { LoginForm } from "../components/Login/LoginForm";
 
 const Login = () => {
   return (
-    <div> Login Page for pathexplorer </div>
-  )
-}
+    <div id="login-main-div" className="loginMainDiv">
+      <div id="image-background" className="imageBackground">
 
-export default Login
+        <div id = "image-header-main-div" className = "imageHeaderMainDiv">
+          <div id="image-header-div">
+            <h1 className='text-light' style = {{"width": "30%", fontSize: "72px"}}>Path Explorer</h1>
+          </div>
+
+          <div id="image-subheader-div">
+            <p className='text-light' style = {{"textAlign": "start", fontSize: "28px"}}>Explore your new career path within Accenture.</p>
+          </div>
+        </div>
+
+      </div>
+
+      <div id="login-form" className="loginForm">
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+
+export default Login;

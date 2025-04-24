7e94dccf4dbbdca5c59200ceaa66786571595921
JUANECL314 <a01198418@tec.mx>
2025-04-24 13:45:00 -0600

    avances

--- src/pages/manager/ManagerDashboard.jsx
+++ src/pages/manager/ManagerDashboard.jsx
@@ -19,34 +19,6 @@ export const ManagerDashboard = () => {
 
   return (
     <div className = "contenedor-Dashboard">
-      <div className = "titulo-Dashboard">
-        <div className = "titulo-boton">
-          <h1>My Project Dashboard</h1>
-          <Link to={"createproject"} className="proyecto-crear">
-            <p>Create new project</p>
-            <i className="bi bi-plus-lg"></i>
-          </Link>
-        </div> 
-        <br/>
-        <h2>Hello Perenganit, we compiled these projects for you:</h2>
-      </div>
-
-      <div className = "buscador-Dashboard">
-        <input type="text" placeholder="Search by keyword"></input>
-        <button className="buscar-boton"><i className="bi bi-search buscador"></i></button>
-        <div className = "buscador-espacio"></div>
-        <button className="filtro-boton"><i className="bi bi-caret-down-fill filtrador"></i></button>
-        <button className="filtro-boton"><i className="bi bi-funnel filtrador"></i></button>
-      </div>
-
-
-    
-      <div className = "proyectos-Dashboard">
-        
-        <DashboardProjectInfo projects={data_projects}/>
-        
-      </div>
-
     </div>
     
   )
